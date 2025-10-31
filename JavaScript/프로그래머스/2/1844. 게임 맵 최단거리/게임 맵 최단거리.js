4a63758c221580a0f9674f8564ef48e187ca32ea
KIM SANG HUN <huhuhunuhuhn@gmail.com>
2025-10-31 10:40:23 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 33.03 ms, Memory: 37.3 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -0,0 +1,37 @@
+function isVisited(value) {
+    return value === 0
+}
+
+function isValidPos(pos, max_pos) {
+    return 0 <= pos && pos <= max_pos
+}
+
+function solution(maps) {
+    const destX = maps[0].length - 1
+    const destY = maps.length - 1
+    
+    const visited = maps.map(inner => [...inner])
+    
+    const queue = [[0, 0, 1]]
+    let pointer = 0
+    while(pointer < queue.length) {
+        const [x, y, count] = queue[pointer++]
+        
+        if(destX === x && destY === y) return count
+        
+        const dx = [0, 0, -1, 1]
+        const dy = [-1, 1, 0, 0]
+        
+        for(let i = 0; i < 4; i++) {
+            const nx = x + dx[i]
+            const ny = y + dy[i]
+            
+            if(isValidPos(nx, destX) && isValidPos(ny, destY) && !isVisited(visited[ny][nx])) {
+                visited[ny][nx] = 0
+                queue.push([nx, ny, count + 1])
+            }
+        }
+    }
+    
+    return -1
+}
