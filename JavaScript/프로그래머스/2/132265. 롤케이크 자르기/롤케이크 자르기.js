076b6bf95fcc505333e1e8075ecfd5295156cf4c
KIM SANG HUN <huhuhunuhuhn@gmail.com>
2025-11-11 10:45:56 +0900

    [level 2] Title: 롤케이크 자르기, Time: 104.35 ms, Memory: 71.2 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ JavaScript/프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -1,53 +1,23 @@
-class Counter {
-    map = new Map()
-    size = 0
-    
-    constructor(initial = []) {
-        initial.forEach((num) => this.append(num))
-        this.size = this.map.size
-    }
-
-    append(number) {
-        if(this.map.has(number)) {
-            this.map.set(number, this.map.get(number) + 1)
-        }
-        else {
-            this.map.set(number, 1)
-            this.size++
-        }
-    }
-
-    pop(number) {
-        const value = this.map.get(number) - 1
-        if (value > 0) {
-            this.map.set(number, value)
-        }
-        else {
-            this.map.delete(number)
-            this.size--
-        }
-        
-        return number
-    }
-    
-    getSize() {
-        return this.size
-    }
-}
-
-function cut(arr, point) {
-    return [arr.slice(0, point), arr.slice(point)]
-}
-
 function solution(topping) {
     var answer = 0
 
-    const a = new Counter()
-    const b = new Counter(topping)
+    const left = new Set()
+    const right = topping.reduce((acc, cur) => { 
+        const value = acc[cur] || 0
+        acc[cur] = value + 1
+        return acc
+    }, {})
+    
+    let rightSize = Object.keys(right).length
 
-    for (let i = 0; i < topping.length - 1; i++) {
-        a.append(b.pop(topping[i]))
-        if (a.getSize() === b.getSize()) answer++
+    for(let i = 0; i < topping.length - 1 && left.size <= rightSize; i++) {
+        const t = topping[i] 
+        left.add(t)
+        right[t] -= 1
+        
+        if(right[t] <= 0) rightSize--
+        
+        if(left.size === rightSize) answer++
     }
 
     return answer
