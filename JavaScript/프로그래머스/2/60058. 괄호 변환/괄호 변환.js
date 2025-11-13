3b9fde92538b0d384599bb2e5d1d5cd8eba3ed62
KIM SANG HUN <huhuhunuhuhn@gmail.com>
2025-11-13 10:11:07 +0900

    [level 2] Title: 괄호 변환, Time: 29.12 ms, Memory: 36 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/2/60058. 괄호 변환/괄호 변환.js
@@ -0,0 +1,57 @@
+const isLeft = (value) => value === '('
+const isRight = (value) => value === ')'
+const isEmpty = (value) => value === ""
+
+const reversal = (p) => p.split('').map(t => isLeft(t) ? ')' : '(').join('')
+
+const isCorrect = (value) => {
+    const stack = []
+    for(let i = 0; i < value.length; i++) {
+        if(isLeft(value[i])) stack.push('(')
+        else if(!isLeft(stack.pop())) return false
+    }
+    return true
+}
+
+const isBalance = (value) => {
+    if(value.length % 2 > 0) return false 
+    
+    const half = value.length / 2
+    let left = 0
+    
+    for(let i = 0; i < value.length; i++) {
+        if(isLeft(value[i])) left++
+        if(left > half) return false
+    }
+    
+    return left === half
+}
+
+const cutParentheses = (p) => {
+    for(let i = 2; i < p.length; i += 2) {
+        const u = p.slice(0, i)
+        const v = p.slice(i)
+    
+        if(isBalance(u) && isBalance(v)) {
+            return [u, v]
+        }
+    }
+    return [p, ""]
+}
+
+const tmp = (w) => {
+    if(isEmpty(w)) return ""
+    
+    const [u, v] = cutParentheses(w)
+    if(isCorrect(u)) return u + tmp(v)
+    
+    let str = "(" + tmp(v) + ")"
+    str += reversal(u.slice(1, u.length-1))
+    return str
+}
+
+function solution(p) {
+    if(isCorrect(p)) return p
+    return tmp(p)
+}
+
