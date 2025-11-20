d01334a6fc80757ba3f8632923372d6cb49da978
KIM SANG HUN <huhuhunuhuhn@gmail.com>
2025-11-20 12:30:38 +0900

    Refactor solution function for largest square search

--- JavaScript/프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.js
+++ JavaScript/프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.js
@@ -1,33 +1,19 @@
 function solution(board) {
-  const n = board.length
-  const m = board[0].length
+    const h = board.length
+    const w = board[0].length
 
-  const dp = Array.from({ length: n }, () => Array(m).fill(0))
+    const dp = Array.from({ length: h }, () => Array(w).fill(0))
 
-  let maxSide = 0
+    let maxSize = 0
+    for (let y = 0; y < h; y++) {
+        for (let x = 0; x < w; x++) {
+            if (board[y][x] === 0) dp[y][x] = 0
+            else if(y === 0 || x === 0)dp[y][x] = 1
+            else dp[y][x] = Math.min(dp[y - 1][x], dp[y][x - 1], dp[y - 1][x - 1]) + 1
 
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < m; j++) {
-
-      if (board[i][j] === 0) {
-        dp[i][j] = 0
-      } else {
-
-        if (i === 0 || j === 0) {
-          dp[i][j] = 1
-        } else {
-          const top = dp[i - 1][j]
-          const left = dp[i][j - 1]
-          const diagonal = dp[i - 1][j - 1]
-          dp[i][j] = Math.min(top, left, diagonal) + 1
+            if (dp[y][x] > maxSize) maxSize = dp[y][x]
         }
-      }
-
-      if (dp[i][j] > maxSide) {
-        maxSide = dp[i][j]
-      }
     }
-  }
 
-  return maxSide * maxSide
+    return maxSize * maxSize
 }
